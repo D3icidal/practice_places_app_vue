39e655ce803571baf995986a42f2847450870072
Thomas Ng <D3icidal@gmail.com>
2018-11-20 19:54:31 -0500

    fixes create action and push to current places array

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,13 @@
   <div class="home">
     <h1>{{ message }}</h1><br>
     <p v-for='place in places'> {{place.name}}.  Located at {{place.address}}</p>
+    <br>
+    <p>New Place
+      Name: <input type='text' v-model="newPlace.name"><br>
+      Address: <input type='text' v-model="newPlace.address">
+      <br>
+      <button v-on:click="addPlace()">Submit Entry</button>
+    </p>
   </div>
 </template>
 
@@ -14,7 +21,8 @@
     data: function() {
       return {
         message: "aWeflcome to Vue.js!",
-        places: []
+        places: [],
+        newPlace: { name: "Empire State Building", address: "One Empire Stree"}
       };
     },
     created: function() {
@@ -24,7 +32,21 @@
       }.bind(this));
     },
 
-    methods: {},
+    methods: {
+      addPlace: function() {
+        var params = {
+          name: this.newPlace.name,
+          address: this.newPlace.address
+        };
+        axios.post('http://localhost:3000/api/places', params).then(function(response) {
+          console.log('inside axios post');
+          console.log(response.data);
+          this.places.push(response.data);
+        }.bind(this));
+        console.log('adding new place - methods > addPlace > post axios');
+        //this.place.push(this.newPlace); //adds the response from the axios-post to the vue array places
+      }
+    },
     computed: {}
   };
 </script>
